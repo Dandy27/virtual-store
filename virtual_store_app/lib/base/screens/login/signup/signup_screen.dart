93b2bdee66a86eddfaf1e86ba780289851d0c053
dandy <danielsbarbosa27@gmail.com>
2021-04-08 17:55:31 -0300

    Validando e salvando os dados

--- virtual_store_app/lib/base/screens/login/signup/signup_screen.dart
+++ virtual_store_app/lib/base/screens/login/signup/signup_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:virtual_store_app/helpers/validators.dart';
+import 'package:virtual_store_app/models/user.dart';
 
 class SignUpScreen extends StatelessWidget {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  final User user = User();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,50 +18,93 @@ class SignUpScreen extends StatelessWidget {
       body: Center(
         child: Card(
           margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            padding: EdgeInsets.all(16),
-            shrinkWrap: true,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Nome Completo'
+          child: Form(
+            key: formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16),
+              shrinkWrap: true,
+              children: [
+                TextFormField(
+                  onSaved: (name) => user.name = name,
+                  validator: (name) {
+                    if (name.isEmpty)
+                      return 'Campo obrigatŕio';
+                    else if (name.trim().split('').length <= 1)
+                      return 'Preencha seu Nome completo';
+                    return null;
+                  },
+                  decoration: const InputDecoration(hintText: 'Nome Completo'),
                 ),
-              ),
-              const SizedBox(height: 16,),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                      hintText: 'E-mail'
-                  )
-              ),
-              const SizedBox(height: 16,),
-              TextFormField(
-                  decoration: const InputDecoration(
-                      hintText: 'Senha'
-                  ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 16,),
-              TextFormField(
-                  decoration: const InputDecoration(
-                      hintText: 'Repitir Senha'
-                  ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 16,),
-              SizedBox(height: 44,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).primaryColor),
-                onPressed: (){},
-                child: const Text(
-                  'Entrar',
-                  style: TextStyle(fontSize: 20),
+                const SizedBox(
+                  height: 16,
                 ),
-              ),
-              ),
+                TextFormField(
+                    onSaved: (email) => user.email = email,
+                    validator: (email) {
+                      if (email.isEmpty)
+                        return 'Campo obrigatório';
+                      else if (!emailValid(email)) return 'E-mail inválido';
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(hintText: 'E-mail')),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  onSaved: (pass) => user.password = pass,
+                  validator: (pass) {
+                    if (pass.isEmpty)
+                      return 'Campo obrigatório';
+                    else if (pass.length < 6) return 'Senha muito curta';
+                    return null;
+                  },
+                  decoration: const InputDecoration(hintText: 'Senha'),
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  onSaved: (pass) => user.confirmPassword = pass,
+                  validator: (pass) {
+                    if (pass.isEmpty)
+                      return 'Campo obrigatório';
+                    else if (pass.length < 6) return 'Senha muito curta';
+                    return null;
+                  },
+                  decoration: const InputDecoration(hintText: 'Repitir Senha'),
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  height: 44,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Theme.of(context).primaryColor),
+                    onPressed: () {
+                      if (formKey.currentState.validate()) {
+                        formKey.currentState.save();
 
-            ],
+                        if (user.password != user.confirmPassword) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text('Senhas não coincidem!'),
+                            backgroundColor: Colors.red,
+                          ));
+                          return;
+                        }
+                      }
+                    },
+                    child: const Text(
+                      'Entrar',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
