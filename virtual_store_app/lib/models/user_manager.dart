bb10e4cd38b7d6f957d7fba37e5b85d3bbad0253
dandy <danielsbarbosa27@gmail.com>
2021-04-08 19:11:14 -0300

    Salvando os Dasos do Usuário

--- virtual_store_app/lib/models/user_manager.dart
+++ virtual_store_app/lib/models/user_manager.dart
@@ -40,10 +40,12 @@ class UserManager extends ChangeNotifier {
       final AuthResult result = await auth.createUserWithEmailAndPassword(
           email: user.email, password: user.password);
 
-        this.user = result.user;
+
+        user.id = result.user.uid;
+
+        await user.saveData();
 
         onSuccess();
-
     } on PlatformException catch (e){
       onFail(getErrorString(e.code));
     }
