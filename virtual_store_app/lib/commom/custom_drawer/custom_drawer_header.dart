d6ea6b0d710f8982633051f4b64b27959b5fe320
dandy <danielsbarbosa27@gmail.com>
2021-04-09 02:51:03 -0300

    Ajustes no Drawer logo

--- virtual_store_app/lib/commom/custom_drawer/custom_drawer_header.dart
+++ virtual_store_app/lib/commom/custom_drawer/custom_drawer_header.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:virtual_store_app/models/user_manager.dart';
@@ -6,54 +7,46 @@ class CustomDrawerHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.fromLTRB(36, 30, 16, 8),
+      padding: const EdgeInsets.fromLTRB(30, 24, 16, 8),
       height: 180,
-      child: Consumer<UserManager>(
-        builder: (_, userManager, __){
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                'SUZZARA',
+      child: Consumer<UserManager>(builder: (_, userManager, __){
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.zero,
+              width: 250,
+              child: Image.asset('images/suzzara.png'),
+            ),
+            Text(
+              'Olá, ${userManager.user?.name ?? ''}',
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            GestureDetector(
+              onTap: () {
+                if (userManager.isLoggedIn) {
+                  userManager.signOut();
+                } else {
+                  Navigator.of(context).pushNamed('/login');
+                }
+              },
+              child: Text(
+                userManager.isLoggedIn ? 'Sair' : 'Entre ou cadastre >',
                 style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-              Text(
-                'Olá ${userManager.user?.name ?? ''}',
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
+                  color: Theme
+                      .of(context)
+                      .primaryColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              GestureDetector(
-                onTap: (){
-                  if(userManager.isLoggedIn){
-                    userManager.signOut();
-                  } else {
-                    Navigator.of(context).pushNamed('/login');
-                  }
-
-                },
-                child: Text(
-                  userManager.isLoggedIn
-                      ? 'Sair'
-                      : 'Entre ou Cadastre-se >',
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-              )
-            ],
-          );
-        },
-      ),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
