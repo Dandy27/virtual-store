c12caa4eb7cab02393a28a6a089ca731de75f4d5
dandy <danielsbarbosa27@gmail.com>
2021-04-09 20:10:35 -0300

    Convertendo o Documento para um produto

--- virtual_store_app/lib/models/product_manager.dart
+++ virtual_store_app/lib/models/product_manager.dart
@@ -1,20 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:virtual_store_app/models/product.dart';
 
-class ProductManager{
+class ProductManager extends ChangeNotifier {
 
-  ProductManager(){
-      _loadAllProducts();
+  ProductManager() {
+    _loadAllProducts();
   }
 
   final Firestore firestore = Firestore.instance;
 
-  Future<void> _loadAllProducts() async{
+  List<Product> _allProducts = [];
+
+  Future<void> _loadAllProducts() async {
     final QuerySnapshot snapProducts =
     await firestore.collection('products').getDocuments();
 
-    for(DocumentSnapshot doc in snapProducts.documents){
-      print(doc.data);
-    }
-
+    _allProducts =
+        snapProducts.documents.map((d) => Product.fromDocument(d)).toList();
   }
+
+  @override
+  void notifyListeners();
+
 }
