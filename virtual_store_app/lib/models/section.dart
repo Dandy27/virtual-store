e198f5797d8991cb480b6dbbd1949035b05dd0ff
dandy <danielsbarbosa27@gmail.com>
2021-04-12 01:22:42 -0300

    Buscando as images das seções

--- virtual_store_app/lib/models/section.dart
+++ virtual_store_app/lib/models/section.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:virtual_store_app/models/sectiom_item.dart';
 
 
 class Section {
@@ -6,9 +7,16 @@ class Section {
   Section.fromDocument(DocumentSnapshot document){
   name = document.data['name'] as String;
   type = document.data['type'] as String;
+  items = (document.data['items'] as List).map((i ) => SectionItem.fromMap(i as Map<String, dynamic>)).toList();
 
   }
 
   String name;
   String type;
+  List<SectionItem> items;
+
+  @override
+  String toString() {
+    return 'Section{name: $name, type: $type, items: $items}';
+  }
 }
