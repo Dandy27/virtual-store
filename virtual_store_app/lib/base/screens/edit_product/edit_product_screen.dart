a5257d9cb69a8596b02e546161826aebfd94a491
dandy <danielsbarbosa27@gmail.com>
2021-04-15 19:12:21 -0300

    Validando o campo de Imagens

--- virtual_store_app/lib/base/screens/edit_product/edit_product_screen.dart
+++ virtual_store_app/lib/base/screens/edit_product/edit_product_screen.dart
@@ -1,25 +1,40 @@
+
 import 'package:flutter/material.dart';
 import 'package:virtual_store_app/models/product.dart';
 
 import 'components/images_form.dart';
 
 class EditProductScreen extends StatelessWidget {
-  const EditProductScreen(this.product);
+  EditProductScreen(this.product);
   final Product product;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Editar Anuncio'
+            'Editar Produto'
         ),
         centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          ImagesForm(product),
-        ],
+      backgroundColor: Colors.white,
+      body: Form(
+        key: formKey,
+        child: ListView(
+          children: [
+            ImagesForm(product),
+            ElevatedButton(onPressed: (){
+              if(formKey.currentState.validate()){
+                print('válido');
+              } else{
+                print('inválido');
+              }
+            },
+                child:const Text('Salvar') )
+          ],
+        ),
       ),
     );
   }
