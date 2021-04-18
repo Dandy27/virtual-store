cbda005887d4a88be1bcb4a987323df2f39b5914
dandy <danielsbarbosa27@gmail.com>
2021-04-18 00:05:31 -0300

    Editando Titulo e Removendo seção

--- virtual_store_app/lib/base/screens/home/components/section_header.dart
+++ virtual_store_app/lib/base/screens/home/components/section_header.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:virtual_store_app/commom/custom_drawer/custom_icon_button.dart';
+import 'package:virtual_store_app/models/home_manager.dart';
 import 'package:virtual_store_app/models/section.dart';
+import 'package:provider/provider.dart';
 
 class SectionHeader extends StatelessWidget {
 
@@ -9,14 +12,45 @@ class SectionHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Text(section.name ?? ' love',
-      style: TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.w800,
-        color: Colors.white
-      ),),
-    );
+
+    final homeManager = context.watch<HomeManager>();
+    if(homeManager.editing){
+      return Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              initialValue: section.name,
+              decoration: InputDecoration(
+                hintText: 'Titulo',
+                isDense: true
+              ),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w800,
+              ),
+              onChanged: (text) => section.name = text,
+            ),
+          ),
+          CustomIconButton(
+            iconData: Icons.remove,
+            color: Colors.white,
+            onTap: (){
+              homeManager.removeSection(section);
+            },
+          )
+        ],
+      );
+    }else{
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Text(section.name ?? ' love',
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w800,
+              color: Colors.white
+          ),),
+      );
+    }
   }
 }
