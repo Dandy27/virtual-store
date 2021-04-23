25a16f09e22a44f3a9c7b4a680c068ce6ded93f6
dandy <danielsbarbosa27@gmail.com>
2021-04-23 14:36:22 -0300

    Gerando número único e incremental de pedido

--- virtual_store_app/lib/models/checkout_manager.dart
+++ virtual_store_app/lib/models/checkout_manager.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:virtual_store_app/models/cart_manager.dart';
 
 class CheckoutManager extends ChangeNotifier{
 
   CartManager cartManager;
+  final Firestore firestore = Firestore.instance;
 
   void updateCart(CartManager cartManager){
     this.cartManager = cartManager;
@@ -14,6 +16,28 @@ class CheckoutManager extends ChangeNotifier{
 
   void checkout(){
     _decrementStock();
+
+    _getOrderId().then((value) => print(value));
+  }
+
+  Future<int> _getOrderId()async {
+    final ref = firestore.document('aux/ordercounter');
+
+ try{
+   final result = await firestore.runTransaction((tx) async{
+     final doc = await tx.get(ref);
+     final orderId = doc.data['current'] as int;
+     await tx.update(ref, {'current' : orderId + 1});
+     return {'orderId' : orderId};
+   });
+   return result['orderId'] as int;
+ }catch(e){
+   debugPrint(e.toString());
+   return Future.error('Falha ao gerar númerro do pedido');
+ }
+
+
+
   }
 
   void _decrementStock(){
