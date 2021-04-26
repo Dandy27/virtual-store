e0bb58d8f098741fa8cbf634893e6cba726d4de0
dandy <danielsbarbosa27@gmail.com>
2021-04-26 02:47:52 -0300

    Exibindo os pedidos

--- virtual_store_app/lib/base/screens/base_screen.dart
+++ virtual_store_app/lib/base/screens/base_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:virtual_store_app/base/screens/admin_users/admin_users_screen.dart';
 import 'package:virtual_store_app/base/screens/home/home_screen.dart';
+import 'package:virtual_store_app/base/screens/orders/orders.screen.dart';
 import 'package:virtual_store_app/base/screens/products/products_screen.dart';
 import 'package:virtual_store_app/commom/custom_drawer/custom_drawer.dart';
 import 'package:virtual_store_app/models/page_manager.dart';
@@ -17,42 +18,35 @@ class _BaseScreenState extends State<BaseScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     return Provider(
       create: (_) => PageManager(pageController),
       child: Consumer<UserManager>(
-        builder: (_, userManager, __){
+        builder: (_, userManager, __) {
           return PageView(
             controller: pageController,
             physics: const NeverScrollableScrollPhysics(),
             children: [
               HomeScreen(),
               ProductScreen(),
+              OrdersScreen(),
               Scaffold(
                 drawer: CustomDrawer(),
                 appBar: AppBar(
                   title: const Text('Home 3'),
                 ),
               ),
-              Scaffold(
-                drawer: CustomDrawer(),
-                appBar: AppBar(
-                  title: const Text('Home 3'),
+              if (userManager.adminEnabled) ...[
+                AdminUserScreen(),
+                Scaffold(
+                  drawer: CustomDrawer(),
+                  appBar: AppBar(
+                    title: const Text('Pedidos'),
+                  ),
                 ),
-              ),
-              if(userManager.adminEnabled)
-                ...[AdminUserScreen(),
-                  Scaffold(
-                    drawer: CustomDrawer(),
-                    appBar: AppBar(
-                      title: const Text('Pedidos'),
-                    ),
-                  ),]
-
+              ]
             ],
           );
         },
-
       ),
     );
   }
